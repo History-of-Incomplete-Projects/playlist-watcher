08a8b530a38d23c460bca10532fe1cd1cd10db52
ahuangJM <ahuang.jm@outlook.com>
2019-01-08 12:27:20 -0700

    update

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ import setuptools
 setup(
     name="playlist-watcher",
     version="1.0.0",
-    description="A library for enabling/disabling new relic alerts",
+    description="A library for watching youtube playlist for updates and download the videos",
     url="https://github.com/independent-projects/playlist-watcher.git",
     author="JingMing Huang",
     author_email="ahuang.jm@outlook.com",
-    packages=setuptools.find_packages(),
+    packages=["watcher"],
     install_requires=[],
     zip_safe=False,
 )
