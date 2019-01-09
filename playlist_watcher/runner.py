48fb5e8da94ca0f4fd8fc8fa84b263b23fd1f374
ahuangJM <ahuang.jm@outlook.com>
2019-01-08 20:00:07 -0700

    unchange

--- playlist_watcher/runner.py
+++ playlist_watcher/runner.py
@@ -3,19 +3,36 @@ from playlist import Playlist
 from sched import scheduler
 from time import time, sleep
 from logging import Logging
-
+import json
+from error import ExpectedInputPlaylistFileNotFoundError
 
 class Runner(object):
-    def __init__(self, youtube_dl_options, playlists, logbook_name):
+    def __init__(self, youtube_dl_options, playlists_name, logbook_name):
         self.runner = youtube_dl.YoutubeDL(youtube_dl_options)
-        self.playlists = playlists
+        self.playlists_name = playlists_name
         self.logger = Logging(logbook_name)
 
     def run(self, period=0):
         s = scheduler(time, sleep)
+        s.enter(period, 2, self.get_playlists, ())
         s.enter(period, 1, self.download_playlists, ())
         while True:
             s.run()
+
+    def get_playlists(self):
+        try:
+            with open(self.playlists_name) as f:
+                playlists = json.load(f)
+                self.playlists = [
+                    Playlist(
+                        playlist["playlist"]["url"],
+                        playlist["playlist"]["name"],
+                        playlist["include_video"],
+                    )
+                    for playlist in playlists["playlists"]
+                ]
+        except (OSError, IOError) as e:
+            raise ExpectedInputPlaylistFileNotFoundError
 
     def download_playlists(self):
         for playlist in self.playlists:
